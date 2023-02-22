3a182fc461c8d77c43339f8e37b9cedd3dba08b3
Mert Çınar <kadirmertcnr@gmail.com>
2023-02-22 12:23:35 +0300

    Basit Menu Eklendi

---
+++ Assets/Scripts/Back2Menu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Back2Menu : MonoBehaviour
+{
+    
+    void Update()
+    {
+        Backtomenu();
+    }
+
+
+    private void Backtomenu()
+    {
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+        
+    }
+}
